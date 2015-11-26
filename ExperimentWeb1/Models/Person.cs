34ddb71558ee7fd932e50008ad1cf5525005bbf4
ar3a51 <ar3a51@yahoo.com>
2015-11-26 20:48:19 +1100

    master

---
+++ ExperimentWeb1/Models/Person.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace ExperimentWeb1.Models
+{
+    public class Person
+    {
+        [Display(Name = "first name")]
+        [Required]
+        public string Name { set; get; }
+
+        [Display(Name = "Last Name")]
+        [Required]
+        public string LastName { set; get; }
+    }
+}
